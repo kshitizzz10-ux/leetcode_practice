afadfea4ed1c5192af4f3e78021cbeeb3d726317
kshitizzz10-ux <kshitiz.goyal1003@gmail.com>
2026-09-03 14:57:33 +0530

    Time: 10 ms (92.07%) | Memory: 35.3 MB (73.17%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        small = min(nums1)
+        nums2 = []
+        if small % 2 == 1:
+            return True
+        for x in nums1:
+            if x % 2 == 1:
+                return False
+        return True
+
