1c35fe53a74ec4c97dee4db21e6d47223195abb6
kshitizzz10-ux <kshitiz.goyal1003@gmail.com>
2026-09-05 20:15:13 +0530

    Time: 147 ms (13.83%) | Memory: 34.7 MB (35.21%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,9 +1,22 @@
 from collections import deque
 class Solution:
     def rotate(self, nums: list[int], k: int) -> None:
-        dqnums = deque(nums)
-        dqnums.rotate(k)
-        nums[:] = dqnums
-
-
-        
+        l = 0
+        r = len(nums) - 1
+        k %= len(nums)
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
+        l = 0 
+        r = k-1
+        while l<r:
+            nums[l], nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
+        l, r = k , len(nums) - 1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
+        return nums
